3684737a8b732bc0a82c47c04d3d6798cd9629bf
시혁 박 <exon.indiv@gmail.com>
2022-08-11 20:09:38 +0900

    Init:

---
+++ build/src/index.js
@@ -0,0 +1,6 @@
+console.log('dd');
+console.log('dd');
+console.log('dd');
+console.log('dd');
+console.log('dd');
+console.log('dd');
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: process.env.BOT_ID,
+		password: process.env.BOT_TOKEN
+	},
+	channels: [ 'exondev' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if (self) return
+
+	if (message.toLowerCase() === '!hello') {
+		// "@alca, heya!"
+		client.say(channel, `@${tags.username}, heya!`)
+	}
+});
+
+// https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=<clien_id>&redirect_uri=http://localhost&scope=chat:read+chat:edit
